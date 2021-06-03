d9bdbb785b702f1c182b3539144666abb1890fd4
mirkasp <79247772+mirkasp@users.noreply.github.com>
2021-06-03 10:57:07 +0200

    refactoring and fixes in GetAppCaption() function

--- dluAppInfo.pas
+++ dluAppInfo.pas
@@ -23,41 +23,40 @@ implementation
 
 uses dluSysInfo
    , dluFileInfo
+{$IFDEF WINDOWS}
+   , Windows
+{$ENDIF}
 {$IFNDEF FPC}
    , SysUtils
    , mkSysInfo
 {$ENDIF}
    ;
 
-const acVersionStr : array[ boolean ] of Char = ( 'w', 'v' );
+//function BoolAsStr( const ACondition: boolean; const ATrueStr, AFalseStr: string ): string; inline;
+//begin
+//   if ACondition then Result := ATrueStr else Result := AFalseStr;
+//end;
 
 function GetAppCaption( const AppShortName: String; const AOptions: TAppCaptionOptions ): String;
-  var s : string;
+  const DEBUG_MODE = {$IFOPT D+} ', DEBUG mode' {$ELSE} '' {$endif};
+        acVersionStr   : array[ boolean ] of Char = ( 'w', 'v' );
+        acInsideIdeStr : array[ boolean ] of String = ( '', ', IDE' );
 begin
-   s := ' ' + acVersionStr[ apcEnglish in AOptions ] + '.';
-{$IFDEF FPC}
    Result := AppShortName;
    if not (apcHideVersion in AOptions) then begin
-      Result := Result + s
-               + dluFileInfo.GetFileInfo().FileVersion {$IFDEF DEBUG} + ' [DEBUG mode]' {$ENDIF}
-               + ' [' + dluSysInfo.CompilerVersionAsString();
-      //if IsInsideDelphiIDE then Append( ' IDE' );
-      Result := Result + ']';
+      Result := Result + ' '
+               + acVersionStr[ apcEnglish in AOptions ] + '.' + dluFileInfo.GetFileVersion()
+               + ' [' + dluSysInfo.CompilerVersionAsString()
+      {$IFDEF FPC}
+               {$IFDEF WINDOWS}
+               + acInsideIdeStr[ IsDebuggerPresent() ]
+               {$ENDIF}
+      {$ELSE}
+               + acInsideIdeStr[ mkSysInfo.IsInsideDelphiIDE() ]
+      {$ENDIF}
+               + DEBUG_MODE
+               + ']';
    end;
-{$ELSE}
-   with TStringBuilder.Create do begin
-      Append( AppShortName );
-      if not (apcHideVersion in AOptions) then begin
-         Append( s );
-         Append( dluFileInfo.GetFileVersion() {$IFDEF DEBUG} + ' [DEBUG mode]' {$ENDIF} );
-         Append( ' [' + dluSysInfo.CompilerVersionAsString() );
-         if mkSysInfo.IsInsideDelphiIDE then Append( ' IDE' );
-         Append( ']' );
-      end;
-      Result := ToString;
-      Free;
-   end;
-{$ENDIF}
 end;
 
 function GetAppCaption( const AppShortName: String; const AHideVersion: boolean ): String;
