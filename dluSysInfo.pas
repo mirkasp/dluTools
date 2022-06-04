0c989f306bf5f32b405d6fda312cde067e435811
mirkasp <79247772+mirkasp@users.noreply.github.com>
2022-06-04 14:07:43 +0200

    localization for GetMemoryInfo

--- dluSysInfo.pas
+++ dluSysInfo.pas
@@ -1,24 +1,20 @@
 unit dluSysInfo;
 
 {$I dluOptions.inc}
-//{$IFDEF FPC}
-//  {$MODE delphiunicode}
-//  {$DEFINE UNICODEDEF}
-//{$ENDIF}
-
 //
 // http://delphiexamples.com/systeminfo/username.html
 //
 
 interface
 
+type TuLocalization = ( ulEn, ulPl );
+
 function CompilerVersionAsString: string;
 
 function GetUserName(): string;
 function GetComputerName(): string;
 
-function GetMemoryInfo(const ABold: boolean = false): string;
-function GetMemoryInfoEx(const ABold: boolean = false): string;
+function GetMemoryInfo(const ABold: boolean = false; const ALng: TuLocalization = ulEn ): string;
 
 //function GetProcessorName(): string;
 // moved to dluTinyProcessor.pas as GetProcessorInfo(...) function !
@@ -44,14 +40,18 @@ uses Windows
   // FPC 3.0 fileinfo reads exe resources as long as you register the appropriate units
   //, fileinfo
   , winpeimagereader {need this for reading exe info}
-  , elfreader {needed for reading ELF executables}
-  , machoreader {needed for reading MACH-O executables}
+  , elfreader        {needed for reading ELF executables}
+  , machoreader      {needed for reading MACH-O executables}
 
 {$ELSE}
   , mkSysInfo
 {$ENDIF}
-//  , uSMBIOS
   ;
+
+type TuLangString = array[ TuLocalization ] of AnsiString;
+
+const aLSTotal : TuLangString = ( 'total', 'razem' );
+const aLSFree  : TuLangString = ( 'free',  'dostępne' );
 
 const aBoldStart: array[ boolean ] of string = ( '', '<b>' );
 const aBoldEnd  : array[ boolean ] of string = ( '', '</b>' );
@@ -102,18 +102,6 @@ begin
 
 end;
 
-function GetMemoryInfo(const ABold: boolean): string;
-  var MS_Ex : TMemoryStatus;
-begin
-  MS_Ex.dwLength := SizeOf( TMemoryStatus );
-  GlobalMemoryStatus( MS_Ex );
-
-  Result := String( Format( '%s%0.0n B%s total, %s%0.0n B%s free',
-                            [ aBoldStart[ ABold ], 1.0 * MS_Ex.dwTotalPhys, aBoldEnd[ ABold ],
-                              aBoldStart[ ABold ], 1.0 * MS_Ex.dwAvailPhys, aBoldEnd[ ABold ]
-                            ] ) );
-end;
-
 {$IFDEF FPC}
 type TMemoryStatusEx = record
   dwLength               : DWORD;
@@ -128,21 +116,24 @@ type TMemoryStatusEx = record
 end;
 
 function GlobalMemoryStatusEx( var AParam:TMemoryStatusEx ): Boolean; stdcall; external 'Kernel32.dll' name 'GlobalMemoryStatusEx';
-
-
 {$ENDIF}
 
 
 
-function GetMemoryInfoEx(const ABold: boolean): string;
+function GetMemoryInfo(const ABold: boolean; const ALng: TuLocalization): string;
   var MS_Ex : TMemoryStatusEx;
 begin
   MS_Ex.dwLength := SizeOf( TMemoryStatusEx );
-  GlobalMemoryStatusEx( MS_Ex );
-  Result := String( Format( '%s%0.0n B%s total, %s%0.0n B%s free',
-                            [ aBoldStart[ ABold ], 1.0 * MS_Ex.ullTotalPhys, aBoldEnd[ ABold ],
-                              aBoldStart[ ABold ], 1.0 * MS_Ex.ullAvailPhys, aBoldEnd[ ABold ]
-                            ] ) );
+  if GlobalMemoryStatusEx( MS_Ex ) then
+     Result := String( Format( '%s%0.0n B%s %s, %s%0.0n B%s %s',
+                               [ aBoldStart[ ABold ], 1.0 * MS_Ex.ullTotalPhys, aBoldEnd[ ABold ],
+                                 aLSTotal[ aLng ],
+                                 aBoldStart[ ABold ], 1.0 * MS_Ex.ullAvailPhys, aBoldEnd[ ABold ],
+                                 aLSFree[ ALng ]
+                               ] ) )
+  else
+      Result := UTF8Decode( SysErrorMessage( GetLastError() ) );
+
 end;
 
 function GetUserName(): string;
