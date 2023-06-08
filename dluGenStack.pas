def8a40695e22f29ada48fb69d6e061de69b0a4b
mirkasp <79247772+mirkasp@users.noreply.github.com>
2023-06-08 08:23:00 +0200

    changing compiler option {$mode Delphi}

--- dluGenStack.pas
+++ dluGenStack.pas
@@ -1,33 +1,28 @@
 unit dluGenStack;
 
-{$I dluOptions.inc}
+//{$I dluOptions.inc}
+{$mode Delphi}{$H+}
 
 interface
 
 uses Classes
    ;
 
-type
-  {$IFDEF FPC}
-
-  { TuStack }
-
-generic{$ENDIF}
-  TuStack<ITEM> = class
+type TuStack<_T> = class
   strict private
      fStartCapacity  : integer;
      fListLen        : integer;
      fNextElem       : integer;
-     fList           : array of ITEM;
+     fList           : array of _T;
   public
      const DEFAULT_START_CAPACITY = 1024;
      constructor Create( const AStartCapacity: integer = DEFAULT_START_CAPACITY );
      //
-     procedure Clear;  {$IFNDEF FPC}inline;{$ENDIF}
-     procedure Push( const xElem: ITEM ); inline;
-     function Pop: ITEM; inline;
-     function IsEmpty: boolean; {$IFNDEF FPC}inline;{$ENDIF}
-     function TryPop( var AElem: ITEM ): boolean;
+     procedure Clear;  inline;
+     procedure Push( const xElem: _T ); inline;
+     function Pop: _T; inline;
+     function IsEmpty: boolean; inline;
+     function TryPop( var AElem: _T ): boolean;
 end;
 
 implementation
@@ -36,26 +31,26 @@ uses SysUtils;
 
 { TuStack<ITEM> }
 
-constructor TuStack{$IFNDEF FPC}<ITEM>{$ENDIF}.Create(const AStartCapacity: integer);
+constructor TuStack<_T>.Create(const AStartCapacity: integer);
 begin
    inherited Create;
    fStartCapacity := AStartCapacity;
    fListLen       := 0;
-   Clear;
+   {%H-}Clear;
    SetLength( fList, fListLen );
 end;
 
-procedure TuStack{$IFNDEF FPC}<ITEM>{$ENDIF}.Clear;
+procedure TuStack<_T>.Clear;
 begin
    fNextElem := 0;
 end;
 
-function TuStack{$IFNDEF FPC}<ITEM>{$ENDIF}.IsEmpty: boolean;
+function TuStack<_T>.IsEmpty: boolean;
 begin
    Result := (fNextElem = 0);
 end;
 
-function TuStack{$IFNDEF FPC}<ITEM>{$ENDIF}.TryPop(var AElem: ITEM): boolean;
+function TuStack<_T>.TryPop(var AElem: _T): boolean;
 begin
    try
       Result := not IsEmpty;
@@ -64,14 +59,14 @@ begin
     end;
 end;
 
-function TuStack{$IFNDEF FPC}<ITEM>{$ENDIF}.Pop: ITEM;
+function TuStack<_T>.Pop: _T;
 begin
-   if IsEmpty() then raise Exception.Create( 'Generic LIFO queue is empty' );
+   if {%H-}IsEmpty() then raise Exception.Create( 'Generic LIFO queue is empty' );
    Dec( fNextElem );
    Result := fList[ fNextElem ];
 end;
 
-procedure TuStack{$IFNDEF FPC}<ITEM>{$ENDIF}.Push(const xElem: ITEM);
+procedure TuStack<_T>.Push(const xElem: _T);
 begin
   if fNextElem = fListLen then begin
      Inc( fListlen, fStartCapacity );
