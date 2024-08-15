e0bb4a6751723ac52529813a0ef588b436084f2d
mirkasp <79247772+mirkasp@users.noreply.github.com>
2024-08-15 08:46:12 +0200

    Windows revision support

--- dluWinVer3.pas
+++ dluWinVer3.pas
@@ -62,7 +62,7 @@ type
      procedure Prepare_Win32_NT_Platform;
      procedure TryReadFromRegistry;
      function GetCompilationInfo: WideString;
-     function GetWin11Version: UnicodeString;
+     function GetWindowsRelease: UnicodeString;
    public
      constructor Create;
      destructor Destroy; override;
@@ -101,7 +101,7 @@ type
      property UBR                  : integer    read fUBR;
      property InstallDate          : TDateTime  read fInstallDate;
      //
-     property Win11Version         : UnicodeString read GetWin11Version;
+     property WindowsRelease       : UnicodeString read GetWindowsRelease;
 
 end;
 
@@ -493,17 +493,35 @@ begin
                    [ fMajorVersion, fMinorVersion, fBuildNumber, s, fCSDVersion ] ) );
 end;
 
-function TWinVerSpec.GetWin11Version: UnicodeString;
+function TWinVerSpec.GetWindowsRelease: UnicodeString;
 begin
    Result := '';
+   // https://learn.microsoft.com/en-us/windows/release-health/release-information
    // https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information
    if (fMajorVersion = 10) and (fMinorVersion = 0) and (fProductType = VER_NT_WORKSTATION) then begin
         case fBuildNumber of
+           // windows 11
            22000 : Result := '21H2';
            22621 : Result := '22H2';
            22631 : Result := '23H2';
            26100 : Result := '24H2';
-            else   Result := '????';
+           // windows 10
+           19045 : Result := '22H2';
+           19044 : Result := '21H2';
+           19043 : Result := '21H1';
+           19042 : Result := '20H2';
+           19041 : Result := '2004';
+           18363 : Result := '1909';
+           18362 : Result := '1903';
+           17763 : Result := '1809';
+           17134 : Result := '1803';
+           16299 : Result := '1709';
+           15063 : Result := '1703';
+           14393 : Result := '1607';
+           10586 : Result := '1511';
+           10240 : Result := '1507';
+
+           else    Result := '????';
         end;
    end;
 end;
@@ -606,8 +624,10 @@ begin
       // https://learn.microsoft.com/en-us/windows/release-health/windows11-release-information
       if fMinorVersion = 0 then begin
          if fProductType = VER_NT_WORKSTATION then begin
-            AppendToNameIf( fBuildNumber >=22000, '11', '10' );
-            fName := Trim( fname + ' ' + GetWin11Version() );
+            if fBuildNumber < 22000
+               then AppendToName( '10' )
+               else AppendToName( '11' );
+            AppendToName( GetWindowsRelease() );
          end else AppendToName( 'Server 2016' );
       end else
          fName := UnknownSystem( 'Unknown Windows version', osi );
