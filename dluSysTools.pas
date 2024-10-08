bba2dbe2f83a153a30eb8fc9f15e196aa4f72902
mirkasp <79247772+mirkasp@users.noreply.github.com>
2024-10-08 10:04:56 +0200

    add ConvertBytes() function

--- dluSysTools.pas
+++ dluSysTools.pas
@@ -9,6 +9,8 @@ function OpenDocument( const ADocumentName : AnsiString    ): boolean; overload;
 
 function xFormat( const AFormat: string; const AParams: array of const ): string;
 
+function ConvertBytes(const ABytes: Int64): UnicodeString;
+
 implementation
 
 uses SysUtils
@@ -27,6 +29,29 @@ uses SysUtils
   , ShellApi
 {$ENDIF}
   ;
+
+
+const DTSD = 1024;
+const aDescription: array[ 1..6 ] of UnicodeString = ( 'Kb', 'Mb', 'Gb', 'Tb', 'Pb', 'Eb' );
+const aPowers     : array[ 1..6 ] of Int64  = ( {Kb} DTSD,
+                                                {Mb} DTSD * DTSD,
+                                                {Gb} DTSD * DTSD * DTSD,
+                                                {Tb} DTSD * DTSD * DTSD * 1014,
+                                                {Pb} DTSD * DTSD * DTSD * DTSD * DTSD,
+                                                {Eb} DTSD * DTSD * DTSD * DTSD * DTSD * DTSD );
+
+function ConvertBytes(const ABytes: Int64): UnicodeString;
+  var i: Integer;
+begin
+   Result := UnicodeFormat('%.0n b',[ ABytes + 0.1 - 0.1 ] );
+   if ABytes >= DTSD then begin
+      i := Low( aPowers );
+      while (ABytes > aPowers[ i+1 ]) and (i < High(aPowers)) do Inc(i);
+      Result := UTF8Decode( FormatFloat('###0.##', ABytes / aPowers[ i ] ) ) + ' ' + aDescription[ i ] +
+                ' ('+Result+')';
+   end;
+end;
+
 
 
 function OpenDocument( const ADocumentName : UnicodeString ): boolean;
