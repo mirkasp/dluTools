ab595742e2b8eb0181b26c93aa62f712235668b3
mirkasp <79247772+mirkasp@users.noreply.github.com>
2025-03-24 19:34:32 +0100

    Update dluCursor.pas
    CurStack late binding (created only before first use)

--- dluCursor.pas
+++ dluCursor.pas
@@ -1,4 +1,5 @@
 unit dluCursor;
+// 2025.03.24 - "CurStack" late binding (created only before first use)
 // 2020.05.15 - generic type TuStack<T>
 // 2019.10.31 - first oficial release
 //
@@ -30,25 +31,29 @@ uses Forms, dluGenStack
 
 type TuCurStack = {$IFDEF FPC}specialize{$ENDIF} TuStack<TCursor>;
 
-var CurStack : TuCurStack;
+var CurStack : TuCurStack = nil;
+
+function GetCurStack: TuCurStack;
+begin
+   if not Assigned( CurStack ) then CurStack := TuCurStack.Create(10);
+   Result := CurStack;
+end;
 
 procedure PushCursor( const NewCursor: TCursor );
 begin
-   CurStack.Push( Screen.Cursor );
+   GetCurStack().Push( Screen.Cursor );
    Screen.Cursor := NewCursor;
 end;
 
 procedure PopCursor();
 begin
-   if CurStack.IsEmpty
+   if GetCurStack().IsEmpty
       then Screen.Cursor := Controls.crDefault
       else Screen.Cursor := CurStack.Pop;
 end;
 
-initialization
-  CurStack := TuCurStack.Create(10);
 
 finalization
-  CurStack.Free;
+  if Assigned( CurStack ) then CurStack.Free;
 
 end.
