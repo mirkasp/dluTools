244bc9a448d24528e364d652d3015c85740207a8
mirkasp <79247772+mirkasp@users.noreply.github.com>
2025-12-23 08:33:09 +0100

    Refactor and extend TRegistryHelper with value iteration
    Refactored TRegistryHelper to improve key iteration and add a new IterateValues method for iterating registry values. Introduced callback types for better flexibility and updated internal logic for Unicode support and robust key handling.

--- dluRegistry.helper.pas
+++ dluRegistry.helper.pas
@@ -4,68 +4,120 @@ unit dluRegistry.helper;
 
 interface
 
-uses Registry
-   ;
-type TPredicatePChar = function( const ABufChar: PChar ): boolean of object;
-type TKeyMethod = procedure( ARegistry: TRegistry; const ASubKey, AKeyName: string ) of object;
+uses Registry, Windows, SysUtils, Classes;
 
-{ TRegistryHelper }
+type
+  { Definicje callbacków - wersje dla metod obiektowych }
+  TKeyMethod    = procedure(ARegistry: TRegistry; const AFulPath, AKeyName: string) of object;
+  TValueMethod  = procedure(ARegistry: TRegistry; const AValueName: string; ADataType: Cardinal) of object;
+  TPredicateKey = function(const AKeyName: string): Boolean of object;
 
-type TRegistryHelper = class helper for TRegistry
-   public
-      procedure IterateKeys( const ASubKey: string; APredicate: TPredicatePChar; AKeyMtd: TKeyMethod );
-end;
+  { TRegistryHelper }
+  TRegistryHelper = class helper for TRegistry
+  public
+    { Iteracja po podkluczach (folderach) }
+    procedure IterateKeys(const ASubKey: string; APredicate: TPredicateKey; AKeyMtd: TKeyMethod);
 
-(*************************
-przykład użycia:
-Reg.RootKey := HKEY_CLASSES_ROOT;
-Reg.Iterate( '\', @Predicate_mtd, 'Content Type', @consumer_mtd );
-
-**************************)
+    { Iteracja po wartościach (wpisach/danych) wewnątrz klucza }
+    procedure IterateValues(const ASubKey: string; AValueMtd: TValueMethod);
+  end;
 
 implementation
 
-uses Windows
-   , SysUtils
-   ;
-
-{ TRegistryHelper }
-
-procedure TRegistryHelper.IterateKeys(const ASubKey: string; APredicate: TPredicatePChar; AKeyMtd: TKeyMethod);
-  var KeyName   : string;
-      NameLen   : DWORD;
-      NameBuffer: array [0..MAX_PATH] of Char;
-      dwIndex   : DWORD;
-      retCode   : Longint;
-      hKeyRoot  : HKEY = 0;
+{ Pomocnicza funkcja do pobierania uchwytu i obsługi logiki otwierania }
+function GetTargetKey(AReg: TRegistry; const ASubKey: string; out ACloseNeeded: Boolean): HKEY;
 begin
-   // Otwieramy klucz rejestru
-   if RegOpenKeyEx(Self.RootKey, PChar(ASubKey), 0, KEY_READ, hKeyRoot) <> ERROR_SUCCESS
-      then Exit;
+   ACloseNeeded := False;
+   Result := Default( HKEY );
+   if ASubKey <> '' then begin
+      // Używamy AReg.Access zamiast KEY_READ dla spójności z obiektem
+      if RegOpenKeyExW(AReg.RootKey, PWideChar(UnicodeString(ASubKey)), 0, AReg.Access, Result) = ERROR_SUCCESS then
+         ACloseNeeded := True
+      else
+         Result := 0;
+   end else begin
+      Result := AReg.CurrentKey;
+      if Result = 0 then
+         Result := AReg.RootKey;
+  end;
+end;
 
-   dwIndex := 0;
+procedure TRegistryHelper.IterateKeys(const ASubKey: string; APredicate: TPredicateKey; AKeyMtd: TKeyMethod);
+  var LIndex      : DWORD;
+      LRetCode    : Longint;
+      LTargetHKey : HKEY;
+      LCloseNeeded: Boolean;
+      LBuffer     : array[0..256] of WideChar; // Unicode version
+      LBufLen     : DWORD;
+      LKeyName    : string;
+      LCurrentPath: string;
+begin
+   if not Assigned(AKeyMtd) then Exit;
+
+   LTargetHKey := GetTargetKey(Self, ASubKey, LCloseNeeded);
+   if LTargetHKey = 0 then Exit;
+
+   // Przygotuj ścieżkę do przekazania w callbacku
+   if ASubKey <> '' then
+      LCurrentPath := ASubKey
+   else
+      LCurrentPath := String(Self.CurrentPath);
+
    try
-      repeat
-         // ustawiamy długość bufora na MAX_PATH
-         NameLen := Length( NameBuffer );
-         retCode := RegEnumKeyEx(hKeyRoot, dwIndex, PChar(@NameBuffer), NameLen, nil, nil, nil, nil);
+      LIndex := 0;
+      while True do begin
+         LBufLen := SizeOf(LBuffer) div SizeOf(WideChar);
+         LRetCode := RegEnumKeyExW(LTargetHKey, LIndex, @LBuffer[0], LBufLen, nil, nil, nil, nil);
 
-         if retCode = ERROR_SUCCESS then begin
-            // filtr przez predykat
-            if (not Assigned(APredicate)) or APredicate(PChar(@NameBuffer)) then begin
-               SetString(KeyName, PChar(@NameBuffer), NameLen);
-               AKeyMtd( Self, ASubKey, KeyName );
-            end;
-            Inc(dwIndex);
-         end else if retCode = ERROR_NO_MORE_ITEMS then
-            Break
-         else
-            raise Exception.CreateFmt('%s (%d)', [ SysErrorMessage(retCode), retCode ] );
-      until False;
+         if LRetCode = ERROR_NO_MORE_ITEMS then Break;
+
+         if LRetCode = ERROR_SUCCESS then begin
+            LKeyName := UTF8Encode(WideString(LBuffer));
+            if (not Assigned(APredicate)) or APredicate(LKeyName) then
+               AKeyMtd(Self, LCurrentPath, LKeyName);
+            Inc(LIndex);
+         end else if LRetCode = ERROR_MORE_DATA then begin
+            // Opcjonalnie: obsługa ekstremalnie długich nazw kluczy (rzadkość)
+            Inc(LIndex);
+         end else
+            Break;
+      end;
    finally
-      RegCloseKey(hKeyRoot);
+      if LCloseNeeded then RegCloseKey(LTargetHKey);
    end;
 end;
 
-end .
+procedure TRegistryHelper.IterateValues(const ASubKey: string; AValueMtd: TValueMethod);
+  var LIndex      : DWORD;
+      LRetCode    : Longint;
+      LTargetHKey : HKEY;
+      LCloseNeeded: Boolean;
+      LBuffer     : array[0..16383] of WideChar; // Max długość nazwy wartości to 16,383 znaków
+      LBufLen     : DWORD;
+      LType       : DWORD;
+begin
+   if not Assigned(AValueMtd) then Exit;
 
+   LTargetHKey := GetTargetKey(Self, ASubKey, LCloseNeeded);
+   if LTargetHKey = 0 then Exit;
+
+   try
+      LIndex := 0;
+      while True do begin
+         LBufLen := SizeOf(LBuffer) div SizeOf(WideChar);
+         LRetCode := RegEnumValueW(LTargetHKey, LIndex, @LBuffer[0], LBufLen, nil, @LType, nil, nil);
+
+         if LRetCode = ERROR_NO_MORE_ITEMS then Break;
+
+         if LRetCode = ERROR_SUCCESS then begin
+            AValueMtd(Self, UTF8Encode(WideString(LBuffer)), LType);
+            Inc(LIndex);
+         end else
+            Break;
+      end;
+   finally
+      if LCloseNeeded then RegCloseKey(LTargetHKey);
+   end;
+end;
+
+end.
