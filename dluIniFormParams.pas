78f1350e9f13bffdff647f729509a7881d69aaf0
mirkasp <79247772+mirkasp@users.noreply.github.com>
2026-01-17 20:37:44 +0100

    Refactor and enhance TFormParamConfig for robustness
    Improves TFormParamConfig by adding parameter validation, supporting both legacy and new formats for form parameters, and making the code more robust and maintainable. Introduces helper methods for parsing and validating form parameters, ensures case-insensitive form ID handling, and adds window state persistence. Also improves error handling and code readability.

--- dluIniFormParams.pas
+++ dluIniFormParams.pas
@@ -4,103 +4,183 @@ unit dluIniFormParams;
 
 interface
 
-uses Classes
-   , Forms
-   , IniFiles
-   ;
+uses Classes, Forms, IniFiles;
 
+{ TFormParamConfig }
 type TFormParamConfig = class
-   strict private
-     const INIT_SIZE = 4;
-     const CLI_FORMS_DEFAULT_SECTION  = 'FORMS';
-       var fForms  : TStringList;
-           CliFormsSection : AnsiString;
-   public
-     constructor Create( const AFormsSection: AnsiString = '' );
-     destructor Destroy; override;
-     //
-     procedure SaveFormParam( AForm: TForm; const AId: AnsiString );
-     procedure RestoreFormParam( AForm: TForm; const AId: AnsiString );
-     //
-     procedure SaveToIni( AIni: TIniFile );
-     procedure RestoreFromIni( AIni: TIniFile );
-end;
+  strict private
+    const INIT_SIZE = 8;  // 8 znaków hex dla Integer (32-bit)
+    const LEGACY_SIZE = 4; // Stary format (16-bit) - dla kompatybilności wstecznej
+    const CLI_FORMS_DEFAULT_SECTION = 'FORMS';
 
+    // Indeksy pól w zapisanym stringu
+    const IDX_LEFT   = 0;
+    const IDX_TOP    = 1;
+    const IDX_WIDTH  = 2;
+    const IDX_HEIGHT = 3;
+    const IDX_STATE  = 4;
+    const PARAM_COUNT = 5;
+
+  strict private
+    FForms: TStringList;
+    CliFormsSection: AnsiString;
+
+    function TryParseFormParams(const AData: AnsiString; out ALeft, ATop, AWidth, AHeight, AState: Integer): Boolean;
+    function IsValidWindowState(AState: Integer): Boolean; inline;
+    function IsValidDimension(AValue: Integer): Boolean; inline;
+
+  public
+    constructor Create(const AFormsSection: AnsiString = '' );
+    destructor Destroy; override;
+
+    procedure SaveFormParam(AForm: TForm; const AId: AnsiString);
+    procedure RestoreFormParam(AForm: TForm; const AId: AnsiString);
+
+    procedure SaveToIni(AIni: TIniFile);
+    procedure RestoreFromIni(AIni: TIniFile);
+  end;
 
 implementation
 
-uses SysUtils
-   ;
+uses SysUtils;
 
 { TFormParamConfig }
 
-constructor TFormParamConfig.Create( const AFormsSection: AnsiString = '' );
+constructor TFormParamConfig.Create(const AFormsSection: AnsiString = '');
 begin
    inherited Create;
-   fForms := TStringList.Create;
-   if AFormsSection <> ''
-      then CliFormsSection := AFormsSection
-      else CliFormsSection := CLI_FORMS_DEFAULT_SECTION;
+   FForms := TStringList.Create;
+   FForms.CaseSensitive := False;
 
+   if AFormsSection <> '' then
+      CliFormsSection := AFormsSection
+   else
+      CliFormsSection := CLI_FORMS_DEFAULT_SECTION;
 end;
 
 destructor TFormParamConfig.Destroy;
 begin
-   fForms.Free;
+   FForms.Free;
    inherited Destroy;
 end;
 
-procedure TFormParamConfig.SaveFormParam( AForm: TForm; const AId: AnsiString);
-  var s : AnsiString;
-      n : integer;
+function TFormParamConfig.IsValidWindowState(AState: Integer): Boolean; inline;
 begin
-
-   s := IntToHex( AForm.Left,   INIT_SIZE ) +
-        IntToHex( AForm.Top,    INIT_SIZE ) +
-        IntToHex( AForm.Width,  INIT_SIZE ) +
-        IntToHex( AForm.Height, INIT_SIZE );
-
-   n := fForms.IndexOfName( Aid );
-   if n < 0
-      then fForms.AddPair( AId, s )
-      else fForms.ValueFromIndex[ n ] := s;
-
+   Result := (AState >= Ord(Low(TWindowState))) and (AState <= Ord(High(TWindowState)));
 end;
 
-procedure TFormParamConfig.RestoreFormParam( AForm: TForm; const AId: AnsiString);
-  var _l, _t, _w, _h : integer;
-      s : AnsiString = '';
+function TFormParamConfig.IsValidDimension(AValue: Integer): Boolean; inline;
 begin
-   if fForms.IndexOfName( AId ) < 0
-      then s := '*'
-      else s := fForms.Values[ AId ];
+   // Wymiary powinny być dodatnie i rozsądne (nie większe niż 32767)
+   Result := (AValue > 0) and (AValue < 32767);
+end;
 
-   if not TryStrToInt( '$' + Copy( s, 1 + 0 * INIT_SIZE, INIT_SIZE ), _l ) or
-      not TryStrToInt( '$' + Copy( s, 1 + 1 * INIT_SIZE, INIT_SIZE ), _t ) or
-      not TryStrToInt( '$' + Copy( s, 1 + 2 * INIT_SIZE, INIT_SIZE ), _w ) or
-      not TryStrToInt( '$' + Copy( s, 1 + 3 * INIT_SIZE, INIT_SIZE ), _h ) then begin
+function TFormParamConfig.TryParseFormParams(const AData: AnsiString; out ALeft, ATop, AWidth, AHeight, AState: Integer): Boolean;
+  var fieldSize: Integer;
+      expectedLength: Integer;
 
-     _l := AForm.Left;
-     _t := AForm.Top;
-     _w := AForm.Width;
-     _h := AForm.Height;
+  function GetHexValue(AIndex: Integer; out AValue: Integer): Boolean;
+    var hexStr: AnsiString;
+  begin
+     hexStr := Copy(AData, 1 + AIndex * fieldSize, fieldSize);
+     Result := TryStrToInt('$' + hexStr, AValue);
+  end;
 
+begin
+   Result := False;
+
+   // Wykryj format: nowy (8 znaków) lub stary (4 znaki)
+   expectedLength := PARAM_COUNT * INIT_SIZE;
+   fieldSize      := INIT_SIZE;
+   if Length(AData) <> expectedLength then begin
+      expectedLength := PARAM_COUNT * LEGACY_SIZE;
+      if Length(AData) = expectedLength then
+         fieldSize := LEGACY_SIZE
+      else
+         Exit; // Nieprawidłowa długość
    end;
-   AForm.SetBounds( _l, _t, _w, _h );
 
+   // Parsuj wszystkie pola
+   if not (GetHexValue(IDX_LEFT,   ALeft  ) and
+           GetHexValue(IDX_TOP,    ATop   ) and
+           GetHexValue(IDX_WIDTH,  AWidth ) and
+           GetHexValue(IDX_HEIGHT, AHeight) and
+           GetHexValue(IDX_STATE,  AState )) then
+      Exit;
+
+   // Walidacja
+   if not (IsValidDimension(AWidth) and IsValidDimension(AHeight)) then
+      Exit;
+
+   if not IsValidWindowState(AState) then
+      Exit;
+
+   Result := True;
 end;
 
-procedure TFormParamConfig.SaveToIni( AIni: TIniFile );
-  var i : integer;
+procedure TFormParamConfig.SaveFormParam(AForm: TForm; const AId: AnsiString);
+  var s  : AnsiString;
+      idx: Integer;
 begin
-   for i:= 0 to fForms.Count-1 do
-       AIni.WriteString( CliFormsSection, fForms.Names[i], fForms.ValueFromIndex[i] );
+   if not Assigned(AForm) then
+      Exit;
+
+   if Trim(AId) = '' then
+      Exit;
+
+   // Koduj parametry formularza
+   s := IntToHex(AForm.Left,   INIT_SIZE) +
+        IntToHex(AForm.Top,    INIT_SIZE) +
+        IntToHex(AForm.Width,  INIT_SIZE) +
+        IntToHex(AForm.Height, INIT_SIZE) +
+        IntToHex(Ord(AForm.WindowState), INIT_SIZE);
+
+   // Dodaj lub zaktualizuj wpis
+   idx := FForms.IndexOfName(AId);
+   if idx < 0
+      then FForms.AddPair(AId, s)
+      else FForms.ValueFromIndex[idx] := s;
+end;
+
+procedure TFormParamConfig.RestoreFormParam(AForm: TForm; const AId: AnsiString);
+  var _l, _t, _w, _h, _state: Integer;
+      s: AnsiString;
+begin
+   if not Assigned(AForm) then
+      Exit;
+
+   // Sprawdź czy istnieje wpis dla tego formularza
+   if FForms.IndexOfName(AId) < 0 then
+      Exit; // Brak zapisanych parametrów - zostaw domyślne
+
+   s := FForms.Values[AId];
+
+   // Spróbuj sparsować parametry (obsługuje stary i nowy format)
+   if TryParseFormParams(s, _l, _t, _w, _h, _state) then begin
+      AForm.SetBounds(_l, _t, _w, _h);
+      AForm.WindowState := TWindowState(_state);
+   end;
+   // Jeśli parsowanie się nie powiodło, pozostaw domyślne wartości
+end;
+
+procedure TFormParamConfig.SaveToIni(AIni: TIniFile);
+var
+  i: Integer;
+begin
+  if not Assigned(AIni) then
+    Exit;
+
+  for i := 0 to FForms.Count - 1 do
+    AIni.WriteString(CliFormsSection, FForms.Names[i], FForms.ValueFromIndex[i]);
 end;
 
 procedure TFormParamConfig.RestoreFromIni(AIni: TIniFile);
 begin
-   AIni.ReadSectionRaw( CliFormsSection, fForms );
+   if not Assigned(AIni) then
+      Exit;
+
+   FForms.Clear;
+   AIni.ReadSectionRaw(CliFormsSection, FForms);
 end;
 
 end.
-
