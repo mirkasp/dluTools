72ceb9074c2813e13cac296968e96585d4e7fe2e
mirkasp <79247772+mirkasp@users.noreply.github.com>
2026-04-09 19:32:56 +0200

    Refactor folder filter parameter internals
    Restructure TuFolderToolParam internals: rename fields and methods for clarity (e.g. FileNameMask -> FilePattern, FMtchFuncList -> FFilterFuncs), replace legacy matching funcs with strategy methods (MatchPatternSingle/Multi/MultiLegacy) and choose strategy in SetFilePattern. Use PathMatchSpecEx with PMSF_MULTIPLE on Win7+ and fall back to per-mask PathMatchSpec on older OSes. Add Matches() to apply mask + all registered filter procs, make AddFilterProc null-safe and ClearFilterProcs clear the list. Also cleaned up external declarations formatting and made IsWin7OrAbove static. Overall refactor improves readability and multi-mask handling.

--- dluFolderToolParam.pas
+++ dluFolderToolParam.pas
@@ -4,7 +4,8 @@ unit dluFolderToolParam;
 
 interface
 
-uses Windows, Classes
+uses Windows
+   , Classes
    , Generics.Collections
    , dluSplitString
    ;
@@ -14,177 +15,170 @@ uses Windows, Classes
 type TWin32FindData = TWin32FindDataW;
 type PWin32FindData = PWin32FindDataW;
 {$ENDIF}
-type TFilterMethod  = function( const AFindData: TWin32FindData ): boolean of object;
 
-type
+// Sygnatura metody-filtru przekazywanej z zewnątrz
+type TFilterMethod = function( const AFindData: TWin32FindData ): boolean of object;
 
 { TuFolderToolParam }
 
- TuFolderToolParam = class
+type TuFolderToolParam = class
    strict private
-     type TIsMatchingFunc = function( const fn: PChar ): boolean of object;
-     type TFuncList       = {$IFDEF FPC}specialize {$ENDIF}TList<TFilterMethod> ;
-     var
-        FMtchFuncList     : TFuncList;
-        FMasterSem        : boolean;    // czy jest zdefiniowany jakikolwiek filtr ?
-        //
-        FFileNameMask     : string;
-        FMaskTabStr       : TUnicodeStringDynArray;
-        FIsMatchingFunc   : TIsMatchingFunc;
-        // technical
-        FFreeOnRelease    : boolean;
-      procedure Initialize();
-      procedure SetFileNameMask(const Value: string);
-      function IsMatchingQ0( const fn: PChar ): boolean;
-      function IsMatchingQ1( const fn: PChar ): boolean;
-      function IsMatchingQ2( const fn: PChar ): boolean;
-      function GetIsEmptyMask: boolean;
-      //
-    public
-      class function IsWin7OrAbove: boolean;
-      constructor Create( const AFileNameMask: string = '' );
-      destructor Destroy; override;
-      property FreeOnRelease   : boolean read FFreeOnRelease write FFreeOnRelease;
-      procedure AddFilterFunc( AFunc: TFilterMethod );
-      procedure ClearFilterFuncs();
-      //
-      function MatchingWith( const AFindData: TWin32FindData ): boolean;
-      //
-      property FileNameMask : string  read FFileNameMask;
-      property IsEmptyMask  : boolean read GetIsEmptyMask;
+      type TMaskMatchFunc  = function( const AFileName: PChar ): boolean of object;
+      type TFilterProcList = {$IFDEF FPC}specialize {$ENDIF}TList<TFilterMethod>;
+
+      var
+         FFilterFuncs   : TFilterProcList;
+         FHasMask       : boolean;      // czy zdefiniowano jakąkolwiek maskę?
+         FFilePattern   : string;
+         FPatternParts  : TUnicodeStringDynArray;
+         FMaskMatchFunc : TMaskMatchFunc;
+         FFreeOnRelease : boolean;
+
+      procedure Initialize;
+      procedure SetFilePattern( const AValue: string );
+      function  GetIsEmptyMask: boolean; inline;
+
+      // Strategie dopasowania maski — dobierane w SetFilePattern:
+      function MatchPatternSingle( const AFileName: PChar ): boolean;       // pojedyncza maska
+      function MatchPatternMulti( const AFileName: PChar ): boolean;        // wiele masek, Win7+
+      function MatchPatternMultiLegacy( const AFileName: PChar ): boolean;  // wiele masek, pre-Win7
+
+   public
+      class function IsWin7OrAbove: boolean; static;
+
+      constructor Create( const AFilePattern: string = '' );
+      destructor  Destroy; override;
+
+      procedure AddFilterProc( AProc: TFilterMethod );
+      procedure ClearFilterProcs;
+
+      // Zwraca TRUE jeśli wpis spełnia maskę ORAZ wszystkie zarejestrowane filtry
+      function Matches( const AFindData: TWin32FindData ): boolean;
+
+      // Jeśli TRUE, właściciel obiektu powinien go zwolnić po użyciu
+      property FreeOnRelease : boolean read FFreeOnRelease write FFreeOnRelease;
+      property FilePattern   : string  read FFilePattern;
+      property IsEmptyMask   : boolean read GetIsEmptyMask;
 end;
 
 implementation
 
 uses SysUtils;
 
+// Flaga dla PathMatchSpecEx — zezwala na wzorce rozdzielane średnikiem
 const PMSF_MULTIPLE = $00000001;
 
-function PathMatchSpec( pszFile, pszSpec: PWideChar ): BOOL; stdcall; external 'shlwapi.dll' name 'PathMatchSpecW';
-function PathMatchSpecEx( pszFile, pszSpec: PWideChar; dwFlags: DWORD ): HRESULT; stdcall; external 'shlwapi.dll' name 'PathMatchSpecExW';
+function PathMatchSpec(
+   pszFile, pszSpec : PWideChar
+): BOOL; stdcall; external 'shlwapi.dll' name 'PathMatchSpecW';
+
+function PathMatchSpecEx(
+   pszFile, pszSpec : PWideChar;
+   dwFlags          : DWORD
+): HRESULT; stdcall; external 'shlwapi.dll' name 'PathMatchSpecExW';
 
 { TuFolderToolParam }
 
 class function TuFolderToolParam.IsWin7OrAbove: boolean;
 begin
-   Result := ((Win32MajorVersion = 6) AND (Win32MinorVersion>= 1))  { Any version 6 equal and above Win7 }
-              OR (Win32MajorVersion>= 7);                             { Any version above 6 }
+   Result := ( (Win32MajorVersion = 6) and (Win32MinorVersion >= 1) )
+          or ( Win32MajorVersion >= 7 );
 end;
 
-constructor TuFolderToolParam.Create(const AFileNameMask: string);
+constructor TuFolderToolParam.Create( const AFilePattern: string );
 begin
    inherited Create;
-   Initialize();
-   SetFileNameMask( AFileNameMask );
+   Initialize;
+   SetFilePattern( AFilePattern );
 end;
 
 procedure TuFolderToolParam.Initialize;
 begin
-   FMtchFuncList   := TFuncList.Create;
-   FFreeOnRelease  := false;
-   FMasterSem      := false;
-   FFileNameMask   := '';
+   FFilterFuncs   := TFilterProcList.Create;
+   FFreeOnRelease := false;
+   FHasMask       := false;
+   FFilePattern   := '';
 end;
 
 destructor TuFolderToolParam.Destroy;
 begin
-   FMtchFuncList.Free;
+   FFilterFuncs.Free;
    inherited;
 end;
 
 function TuFolderToolParam.GetIsEmptyMask: boolean;
 begin
-   Result := (FFileNameMask = '') or (FFileNameMask= '*') or (FFileNameMask='*.*')
+   Result := (FFilePattern = '')
+          or (FFilePattern = '*')
+          or (FFilePattern = '*.*');
 end;
 
-//function TuFolderToolParam.MatchingWith( const AFindData: TWin32FindData): boolean;
-//   var i : integer;
-//begin
-//   if FMasterSem and Assigned( FIsMatchingFunc )
-//      then Result := FIsMatchingFunc( @AFindData.cFileName[0] )
-//      else Result := true;
-//   if not Result then Exit;
-//
-//   i := FMtchFuncList.Count - 1;
-//   while Result and (i >= 0 ) do begin
-//      Result := FMtchFuncList[i]( AFindData );
-//      Dec(i);
-//   end;
-//end;
-
-function TuFolderToolParam.MatchingWith(const AFindData: TWin32FindData): boolean;
-   var fm: TFilterMethod;
+procedure TuFolderToolParam.SetFilePattern( const AValue: string );
 begin
-   // Dopasowanie maski
-   if FMasterSem and Assigned(FIsMatchingFunc)
-      then Result := FIsMatchingFunc(@AFindData.cFileName[0])
+   FFilePattern := AValue;
+
+   if GetIsEmptyMask then
+      FPatternParts := nil
+   else
+      FPatternParts := dluSplitString.SplitString( AValue, ';' );
+
+   FHasMask := Length( FPatternParts ) > 0;
+
+   if Length( FPatternParts ) <= 1 then
+      FMaskMatchFunc := {$IFDEF FPC}@{$ENDIF}MatchPatternSingle
+   else if IsWin7OrAbove then
+      FMaskMatchFunc := {$IFDEF FPC}@{$ENDIF}MatchPatternMulti
+   else
+      FMaskMatchFunc := {$IFDEF FPC}@{$ENDIF}MatchPatternMultiLegacy;
+end;
+
+function TuFolderToolParam.Matches( const AFindData: TWin32FindData ): boolean;
+   var fp: TFilterMethod;
+begin
+   if FHasMask and Assigned( FMaskMatchFunc )
+      then Result := FMaskMatchFunc( @AFindData.cFileName[0] )
       else Result := true;
 
    if not Result then
-      exit;
+      Exit;
 
-   // Wszystkie filtry muszą zwrócić TRUE
-   for fm in FMtchFuncList do
-      if not fm(AFindData) then
-         Exit(FALSE);
+   // Wszystkie zarejestrowane filtry muszą zwrócić TRUE
+   for fp in FFilterFuncs do
+      if not fp( AFindData ) then
+         Exit( false );
 end;
 
-
-
-procedure TuFolderToolParam.AddFilterFunc(AFunc: TFilterMethod);
+procedure TuFolderToolParam.AddFilterProc( AProc: TFilterMethod );
 begin
-   FMtchFuncList.Add( AFunc );
+   if Assigned( AProc ) then
+      FFilterFuncs.Add( AProc );
 end;
 
-procedure TuFolderToolParam.ClearFilterFuncs;
+procedure TuFolderToolParam.ClearFilterProcs;
 begin
-   FMtchFuncList.Clear;
+   FFilterFuncs.Clear;
 end;
 
-procedure TuFolderToolParam.SetFileNameMask(const Value: string);
+function TuFolderToolParam.MatchPatternSingle( const AFileName: PChar ): boolean;
 begin
-   FFileNameMask := Value;
-   if GetIsEmptyMask()
-         then FMaskTabStr := nil
-         else FMaskTabStr := dluSplitString.SplitString( Value, ';' );
-
-   FMasterSem := Length(FMaskTabStr) > 0;
-
-   if Length( FMaskTabStr ) <= 1 then begin
-
-      FIsMatchingFunc := {$IFDEF FPC}@{$ENDIF}IsMatchingQ1;
-
-   end else if IsWin7OrAbove() then begin
-
-      FIsMatchingFunc := {$IFDEF FPC}@{$ENDIF}IsMatchingQ2;
-
-   end else begin
-
-      FIsMatchingFunc := {$IFDEF FPC}@{$ENDIF}IsMatchingQ0;
-
-   end;
-
+   Result := PathMatchSpec( AFileName, PChar( FFilePattern ) );
 end;
 
-function TuFolderToolParam.IsMatchingQ0(const fn: PChar): boolean;
-  var i : integer;
+function TuFolderToolParam.MatchPatternMulti( const AFileName: PChar ): boolean;
+begin
+   // PathMatchSpecEx obsługuje wzorce rozdzielane średnikiem natywnie (Win7+)
+   Result := PathMatchSpecEx( AFileName, PChar( FFilePattern ), PMSF_MULTIPLE ) = S_OK;
+end;
+
+function TuFolderToolParam.MatchPatternMultiLegacy( const AFileName: PChar ): boolean;
+   var i: integer;
 begin
    Result := false;
-   i      := Low( FMaskTabStr );
-   while not Result and (i <= High(FMaskTabStr)) do
-      if PathMatchSpec( fn, PChar( FMaskTabStr[i] ) )
-         then Result := true
-         else Inc(i);
-end;
-
-function TuFolderToolParam.IsMatchingQ1( const fn: PChar) : boolean;
-begin
-   Result := PathMatchSpec( fn, PChar( FFileNameMask ) );
-end;
-
-function TuFolderToolParam.IsMatchingQ2( const fn: PChar) : boolean;
-begin
-   Result := PathMatchSpecEx( fn, PChar( FFileNameMask ), PMSF_MULTIPLE ) = S_OK;
+   i      := Low( FPatternParts );
+   while not Result and ( i <= High( FPatternParts ) ) do begin
+      Result := PathMatchSpec( AFileName, PChar( FPatternParts[i] ) );
+      Inc( i );
+   end;
 end;
 
 end.
